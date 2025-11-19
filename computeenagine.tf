f7f81e905250e337e1a78424dec87d1fe3516121
182techlearn <techlearn182@gmail.com>
2025-11-19 16:00:55 +0000

    feat:removed gui vm

--- computeenagine.tf
+++ computeenagine.tf
@@ -36,11 +36,6 @@ resource "tls_private_key" "ssh_key1" {
   rsa_bits  = 2048
 }
 
-# 📦 Get Latest Ubuntu Image with GUI Support
-data "google_compute_image" "ubuntu_gui" {
-  family  = "ubuntu-2204-lts"
-  project = "ubuntu-os-cloud"
-}
 
 # 🌐 Create VPC Network
 resource "google_compute_network" "vpc" {
@@ -59,43 +54,4 @@ resource "google_compute_firewall" "allow_ports" {
   }
 
   source_ranges = ["0.0.0.0/0"]
-}
-
-# 🖥️ Create Ubuntu GUI VM
-resource "google_compute_instance" "ubuntu_gui" {
-  name         = "ubuntu-gui-vm"
-  machine_type = "e2-medium"
-  zone         = "asia-south1-a"
-
-  boot_disk {
-    initialize_params {
-      image = data.google_compute_image.ubuntu_gui.self_link
-      size  = 30
-    }
-  }
-
-  network_interface {
-    network = google_compute_network.vpc.name
-    access_config {} # Enables external IP
-  }
-
-  metadata = {
-    ssh-keys       = "ubuntu:${tls_private_key.ssh_key1.public_key_openssh}"
-    startup-script = <<-EOF
-      #!/bin/bash
-      apt-get update
-      DEBIAN_FRONTEND=noninteractive apt-get install -y ubuntu-desktop
-      apt-get install -y nginx
-      systemctl enable nginx
-      systemctl start nginx
-    EOF
-  }
-
-  tags = ["ubuntu-gui"]
-}
-
-# 💾 Save Private Key Locally
-resource "local_file" "private_key" {
-  content  = tls_private_key.ssh_key1.private_key_pem
-  filename = "${path.module}/ubuntu_gui_key.pem"
 }
